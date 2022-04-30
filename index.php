da9d36ba4bfe18008a6ecf52bc326b54b775ceda
Kate Dinh <79945906+kqteDinh@users.noreply.github.com>
2022-04-30 11:32:25 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<title>Then and Now</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <Link rel ="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<body style="background-color:#fcf4e1;">
+<!--<link href='http://fonts.googleapis.com/css?family=TimesNewRoman:400,300,700' rel='stylesheet' type='text/css'>-->
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #a4c796;">
+  <!-- Navbar content,,, also try this color instead #9DB597-->
+  <div class='nav-brand'> 
+ <a href='index.php'> 
+   <img src="images/mylogo.png" alt='Official logo' width='65px' height='60px'> 
+ </a> 
+</div> 
+  <a class="navbar-brand" href="#">Then and Now</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    <li class="nav-item active">
+        <a class="nav-link" href='objective.php'>Objective <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href='calculator.php'>Calculator <span class="sr-only">(current)</span></a>
+        </li>
+      <li class="nav-item active">
+        <a class="nav-link" href='aboutme.php'>About Me <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href='studytime.php'>Study Time <span class="sr-only">(current)</span></a>
+        </li>
+    </ul>
+
+ </a> 
+</div> 
+  </div>
+</nav>
+
+<!--this needs to stay at the end of the body-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.51/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.52/js/bootstrap.min.js"></script>
+</body>
+<!-- Footer -->
+<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-#fcf4e1 text-black-50">
+    <div class="container text-center">
+      <small>Copyright 2022 &copy; Kate Dinh- All Rights Reserved</small>
+    </div>
+  </footer>
+<!-- Footer -->
+</html>
---
+++ objective.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<title>Then and Now</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <Link rel ="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<body style="background-color:#fcf4e1;">
+<!--<link href='http://fonts.googleapis.com/css?family=TimesNewRoman:400,300,700' rel='stylesheet' type='text/css'>-->
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #a4c796;">
+  <!-- Navbar content,,, also try this color instead #9DB597-->
+  <div class='nav-brand'> 
+ <a href='index.php'> 
+   <img src="images/mylogo.png" alt='Official logo' width='65px' height='60px'> 
+ </a> 
+</div> 
+  <a class="navbar-brand" href="#">Then and Now</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    <li class="nav-item active">
+        <a class="nav-link" href='objective.php'>Objective <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href='calculator.php'>Calculator <span class="sr-only">(current)</span></a>
+        </li>
+      <li class="nav-item active">
+        <a class="nav-link" href='aboutme.php'>About Me <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href='studytime.php'>Study Time <span class="sr-only">(current)</span></a>
+        </li>
+    </ul>
+   
+ </a> 
+</div> 
+  </div>
+</nav>
+<p class="font-weight-bold"> </p>
+<p class="font-weight-bold">  </p>
+<p class="font-weight-bold text-center">The Problem:</p>
+<figure class="text-center">
+  <img src="images/statistics.jpg" class="figure-img img-fluid rounded" alt="statistic figure placeholder">
+  <figcaption class="figure-caption">Results from 2014 Student Psyche Report</figcaption>
+</figure>
+<p class="text-center">- 85% of students say that the pandemic has had a negative effect on their college performance</p>
+<p class="text-center">  - The pandemic has not only affected educational performance, but also mental health</p>
+<p class="text-center">  - 38% of students surveyed said that focusing on work/studies was their main source of stress</p>
+<!-- Footer -->
+<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-#fcf4e1 text-black-50">
+    <div class="container text-center">
+      <small>Copyright 2022 &copy; Kate Dinh- All Rights Reserved</small>
+    </div>
+  </footer>
+<!-- Footer -->
+<!--this needs to stay at the end of the body-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.51/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.52/js/bootstrap.min.js"></script>
+</body>
+</html>
